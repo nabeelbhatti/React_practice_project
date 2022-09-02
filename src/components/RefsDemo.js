f4bfea86e6cd011adf009c265d007cd96b1a83c8
muhammad bilal <nabeelbhatti9393@gmail.com>
2022-09-02 16:24:39 +0500

    refs with call backs

--- src/components/RefsDemo.js
+++ src/components/RefsDemo.js
@@ -4,15 +4,18 @@ export default class RefsDemo extends Component {
   constructor(props) {
     super(props);
     this.inputRef = React.createRef();
-    this.cbRef = null
-    this.setCbRef = element =>{
-        this.cbRef = element
-    }
+    this.cbRef = null;
+    this.setCbRef = (element) => {
+      this.cbRef = element;
+    };
   }
 
   componentDidMount = () => {
-    this.inputRef.current.focus();
-    console.log(this.inputRef);
+    if (this.cbRef) {
+      this.cbRef.focus();
+    }
+    // this.inputRef.current.focus();
+    // console.log(this.inputRef);
   };
 
   handleInputChange = () => {
@@ -23,6 +26,7 @@ export default class RefsDemo extends Component {
     return (
       <div>
         <input type="text" ref={this.inputRef}></input>
+        <input type="text" ref={this.setCbRef}></input>
         <button onClick={this.handleInputChange}>Click</button>
       </div>
     );
